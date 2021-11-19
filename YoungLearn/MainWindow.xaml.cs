7af62cfa9bfa7a5483949fb7a811fdd36f675995
miaochunhuaixia <1608482118@qq.com>
2021-11-19 11:06:03 +0800

    修改了默认框架

--- YoungLearn/MainWindow.xaml.cs
+++ YoungLearn/MainWindow.xaml.cs
@@ -4,24 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.Windows.Forms.Integration;
 using System.Drawing;
 using System.Data;
 using System.Configuration;
-using YoungLearn.Utility;
-using YoungLearn.MessageWindows;
+
 using System.Text.RegularExpressions;
 using System.Windows.Forms.DataVisualization.Charting;
 using AutoUpdaterDotNET;
+
+using YoungLearn.Utility;
+using YoungLearn.MessageWindows;
 
 
 namespace YoungLearn
